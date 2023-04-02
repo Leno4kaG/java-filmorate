1ee96aca6c25c72df69e05c0747e356b4fc3941c
Леночка <leno4kagaripova@yandex.ru>
2023-04-03 01:08:11 +0700

    ТЗ-11. Добавление подключения к БД H2.

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,10 +2,7 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.jdbc.Sql;
-@Sql(
-		scripts = "classpath:filmorate.sql"
-)
+
 @SpringBootApplication
 public class FilmorateApplication {
 
